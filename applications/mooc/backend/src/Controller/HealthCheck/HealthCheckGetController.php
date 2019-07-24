931a8ec8b9bfdc6745af4cc63ece89b7a9eec991
Rafa Gómez <rgomezcasas@gmail.com>
2019-07-24 22:58:04 +0200

    Add autowire

--- applications/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
+++ applications/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -4,17 +4,26 @@ declare(strict_types = 1);
 
 namespace CodelyTv\Mooc\Backend\Controller\HealthCheck;
 
+use CodelyTv\Shared\Infrastructure\RandomNumberGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class HealthCheckGetController
 {
+    private $generator;
+
+    public function __construct(RandomNumberGenerator $generator)
+    {
+        $this->generator = $generator;
+    }
+
     public function __invoke(Request $request): Response
     {
         return new JsonResponse(
             [
                 'mooc-backend' => 'ok',
+                'rand'         => $this->generator->generate(),
             ]
         );
     }
