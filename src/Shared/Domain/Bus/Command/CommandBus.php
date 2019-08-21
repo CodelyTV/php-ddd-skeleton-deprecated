d37653c586033fc2d0250d4bd208ded8ff22efc6
Rafa Gómez <rgomezcasas@gmail.com>
2019-08-22 01:35:01 +0200

    Add command bus

---
+++ src/Shared/Domain/Bus/Command/CommandBus.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodelyTv\Shared\Domain\Bus\Command;
+
+interface CommandBus
+{
+    public function dispatch(Command $command): void;
+}
---
+++ src/Shared/Infrastructure/Bus/Command/CommandNotRegisteredError.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodelyTv\Shared\Infrastructure\Bus\Command;
+
+use CodelyTv\Shared\Domain\Bus\Command\Command;
+use RuntimeException;
+
+final class CommandNotRegisteredError extends RuntimeException
+{
+    public function __construct(Command $command)
+    {
+        $commandClass = get_class($command);
+
+        parent::__construct("The command <$commandClass> hasn't a command handler associated");
+    }
+}
+
