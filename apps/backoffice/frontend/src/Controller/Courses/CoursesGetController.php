7f2b4dba704b0b83e8c12e97a99e129aa1294787
Rafa Gómez <rgomezcasas@gmail.com>
2019-09-18 12:21:10 +0200

    [backoffice-frontend] Add courses counter element with real data

--- apps/backoffice/frontend/src/Controller/Courses/CoursesGetController.php
+++ apps/backoffice/frontend/src/Controller/Courses/CoursesGetController.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace CodelyTv\Apps\Backoffice\Frontend\Controller\Courses;
 
+use CodelyTv\Mooc\CoursesCounter\Application\Find\CoursesCounterResponse;
+use CodelyTv\Mooc\CoursesCounter\Application\Find\FindCoursesCounterQuery;
 use CodelyTv\Shared\Infrastructure\Symfony\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +14,15 @@ final class CoursesGetController extends Controller
 {
     public function __invoke(Request $request): Response
     {
+        /** @var CoursesCounterResponse $coursesCounterResponse */
+        $coursesCounterResponse = $this->ask(new FindCoursesCounterQuery());
+
         return $this->render(
             'pages/courses.html.twig',
             [
                 'title'           => 'Courses',
                 'description'     => 'Courses CodelyTV - Backoffice',
-                'courses_counter' => 5,
+                'courses_counter' => $coursesCounterResponse->total(),
             ]
         );
     }
--- src/Shared/Infrastructure/Symfony/Controller.php
+++ src/Shared/Infrastructure/Symfony/Controller.php
@@ -4,20 +4,30 @@ declare(strict_types = 1);
 
 namespace CodelyTv\Shared\Infrastructure\Symfony;
 
-use Symfony\Component\HttpFoundation\Response;
+use CodelyTv\Shared\Domain\Bus\Query\Query;
+use CodelyTv\Shared\Domain\Bus\Query\QueryBus;
+use CodelyTv\Shared\Domain\Bus\Query\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Twig\Environment;
 
 abstract class Controller
 {
     private $twig;
+    private $queryBus;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, QueryBus $queryBus)
     {
-        $this->twig = $twig;
+        $this->twig     = $twig;
+        $this->queryBus = $queryBus;
     }
 
-    public function render(string $templatePath, array $arguments = []): Response
+    public function render(string $templatePath, array $arguments = []): SymfonyResponse
     {
-        return new Response($this->twig->render($templatePath, $arguments));
+        return new SymfonyResponse($this->twig->render($templatePath, $arguments));
+    }
+
+    public function ask(Query $query): ?Response
+    {
+        return $this->queryBus->ask($query);
     }
 }
