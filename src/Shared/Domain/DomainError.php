8884f41397928a65cef6c146396eabb55c6f183b
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-21 08:14:40 +0200

    Add error handling

---
+++ src/Shared/Domain/DomainError.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodelyTv\Shared\Domain;
+
+use DomainException;
+
+abstract class DomainError extends DomainException
+{
+    public function __construct()
+    {
+        parent::__construct($this->errorMessage());
+    }
+
+    abstract public function errorCode(): string;
+
+    abstract protected function errorMessage(): string;
+}
--- src/Shared/Infrastructure/Symfony/WebController.php
+++ src/Shared/Infrastructure/Symfony/WebController.php
@@ -4,11 +4,8 @@ declare(strict_types = 1);
 
 namespace CodelyTv\Shared\Infrastructure\Symfony;
 
-use CodelyTv\Shared\Domain\Bus\Command\Command;
 use CodelyTv\Shared\Domain\Bus\Command\CommandBus;
-use CodelyTv\Shared\Domain\Bus\Query\Query;
 use CodelyTv\Shared\Domain\Bus\Query\QueryBus;
-use CodelyTv\Shared\Domain\Bus\Query\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
@@ -17,26 +14,25 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Twig\Environment;
 
-abstract class WebController
+abstract class WebController extends ApiController
 {
     private $twig;
     private $router;
     private $session;
-    private $queryBus;
-    private $commandBus;
 
     public function __construct(
         Environment $twig,
         RouterInterface $router,
         SessionInterface $session,
         QueryBus $queryBus,
-        CommandBus $commandBus
+        CommandBus $commandBus,
+        ApiExceptionsHttpStatusCodeMapping $exceptionHandler
     ) {
-        $this->twig       = $twig;
-        $this->router     = $router;
-        $this->session    = $session;
-        $this->queryBus   = $queryBus;
-        $this->commandBus = $commandBus;
+        parent::__construct($queryBus, $commandBus, $exceptionHandler);
+
+        $this->twig    = $twig;
+        $this->router  = $router;
+        $this->session = $session;
     }
 
     public function render(string $templatePath, array $arguments = []): SymfonyResponse
@@ -65,16 +61,6 @@ abstract class WebController
         $this->addFlashFor('inputs', $request->request->all());
 
         return new RedirectResponse($this->router->generate($routeName), 302);
-    }
-
-    public function ask(Query $query): ?Response
-    {
-        return $this->queryBus->ask($query);
-    }
-
-    public function dispatch(Command $command): void
-    {
-        $this->commandBus->dispatch($command);
     }
 
     private function formatFlashErrors(ConstraintViolationListInterface $violations): array
