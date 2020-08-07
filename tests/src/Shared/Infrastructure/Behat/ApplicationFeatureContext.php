365705a2a2a341202cad86f46979c54e523f929f
Joel Coll Costa <joel.coll@gmail.com>
2020-08-07 20:59:02 +0200

    Remove unused includes.

--- tests/src/Shared/Infrastructure/Behat/ApplicationFeatureContext.php
+++ tests/src/Shared/Infrastructure/Behat/ApplicationFeatureContext.php
@@ -6,7 +6,6 @@ namespace CodelyTv\Tests\Shared\Infrastructure\Behat;
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
-use Behat\Testwork\Hook\Scope\AfterSuiteScope;
 use CodelyTv\Shared\Infrastructure\Bus\Event\DomainEventJsonDeserializer;
 use CodelyTv\Shared\Infrastructure\Bus\Event\InMemory\InMemorySymfonyEventBus;
 use CodelyTv\Shared\Infrastructure\Doctrine\DatabaseConnections;
